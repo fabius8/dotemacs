da5a4e0228de4807a9b44a55c821a84a0d56592b
fabius8 <fabius8@163.com>
2016-03-17 12:37:38 +0800

    --wip--

---
+++ elpa/adoc-mode-20160314.1430/adoc-mode-autoloads.el
@@ -0,0 +1,27 @@
+;;; adoc-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "adoc-mode" "adoc-mode.el" (22250 10476 0 0))
+;;; Generated autoloads from adoc-mode.el
+
+(autoload 'adoc-mode "adoc-mode" "\
+Major mode for editing AsciiDoc text files.
+Turning on Adoc mode runs the normal hook `adoc-mode-hook'.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("adoc-mode-pkg.el") (22250 10476 288035
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; adoc-mode-autoloads.el ends here
