75079b3f5b80c217e27db1a7aecb3e67802d7e04
fangzheng <fangzheng@gbcom.com.cn>
2016-03-21 13:33:08 +0800

    --wip--

--- elpa/alpha-1.0/alpha-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; alpha-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("alpha.el") (22255 33933 589817 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; alpha-autoloads.el ends here
