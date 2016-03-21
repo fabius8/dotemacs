4c0cd6b7b81bb24c7d0848830867035e9766a1bf
fangzheng <fangzheng@gbcom.com.cn>
2016-03-21 14:20:10 +0800

    auto update

--- init.el
+++ init.el
@@ -42,6 +42,15 @@
 ; sr-speedbar 查看c代码定义
 (require 'sr-speedbar)
 (global-set-key (kbd "<f5>") 'sr-speedbar-toggle)
+(defun select-next-window ()
+  (other-window 1))
+
+(defun my-sr-speedbar-open-hook ()
+  (add-hook 'speedbar-before-visiting-file-hook 'select-next-window t)
+  (add-hook 'speedbar-before-visiting-tag-hook 'select-next-window t)
+  )
+
+(advice-add 'sr-speedbar-open 'after 'my-sr-speedbar-open-hook)
 
 ;which-key
 (require 'which-key)
