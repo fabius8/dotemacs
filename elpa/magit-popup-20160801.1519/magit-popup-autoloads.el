b58b0fab48688d8fac6cdac0ddf870ac8bf827dd
cloud <fabius8@163.com>
2016-08-04 16:29:19 +0800

    --wip--

--- elpa/magit-popup-20160801.1519/magit-popup-autoloads.el
+++ elpa/magit-popup-20160801.1519/magit-popup-autoloads.el
@@ -3,6 +3,11 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (22434 64647 18687 350000))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
